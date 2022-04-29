eca31c3137f7932c790d94ac2332df2db6d13684
mankow58047 <mankow58047@gmail.com>
2022-04-29 10:15:44 -0500

    1 file modified

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -42,46 +42,4 @@ export class ContactService {
     this.intContacts[id] = contact;
   }
 
-
-  // private contactURL = 'api/contacts';
-
-  // private intContactsUrl = 'api/intContacts';
-  // log: any;
-
-  // getContacts(): Observable<Contact[]>{
-  //   return this.http.get<Contact[]>(this.intContactsUrl)
-  //     .pipe(
-  //       catchError(this.handleError<Contact[]>('getContacts', []))
-  //     );
-  // }
-
-  // // getContacts(fName: any): Observable<Contact> {
-  // //   const url = `${this.intContactsUrl}/${fName}`;
-  // //   return this.http.get<Contact>(url)
-  // //    .pipe(tap=>
-  // //     catchError(this.handleError<Contact>(`getContacts fName=${fName}`)))
-
-  // // }
-
-  // httpOptions = {
-  //   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-  // };
-
-  // private handleError<T>(operation = 'operation', result?: T) {
-  //   return (error: any): Observable<T> => {
-  
-  //     // TODO: send the error to remote logging infrastructure
-  //     console.error(error); // log to console instead
-  
-  //     // TODO: better job of transforming error for user consumption
-  //     this.log(`${operation} failed: ${error.message}`);
-  
-  //     // Let the app keep running by returning an empty result.
-  //     return of(result as T);
-  //   };
-  // }
-
-
-
-
 }
